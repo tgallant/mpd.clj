b620a39923b3abd6bb398394149ff34d4267cf11
Tim Gallant <me@timgallant.us>
2015-03-01 02:46:56 -0700

    updating docs

--- src/mpd_clj/core.clj
+++
@@ -1,8 +0,0 @@
-(ns mpd-clj.core
-  (:require [mpd-clj.options :refer :all]
-            [mpd-clj.playback :refer :all]
-            [mpd-clj.controlls :refer :all]
-            [mpd-clj.stored :refer :all]
-            [mpd-clj.db :refer :all]))
-
-
--- src/mpd_clj/db.clj
+++
@@ -1,4 +0,0 @@
-(ns mpd-clj.db
-  "the music database http://www.musicpd.org/doc/protocol/database.html"
-  (:require [clojure.string :as str]
-            [mpd-clj.utils :refer [send-cmd]]))
