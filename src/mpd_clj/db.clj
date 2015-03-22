cf919b80b6c417db7bf3364dfbf029abb174c6fb
Tim Gallant <me@timgallant.us>
2015-03-21 20:18:05 -0700

    removing cyclic deps

--- src/mpd_clj/db.clj
+++ src/mpd_clj/db.clj
@@ -1,5 +1,4 @@
 (ns mpd-clj.db
   "the music database http://www.musicpd.org/doc/protocol/database.html"
   (:require [clojure.string :as str]
-            [mpd-clj.core :as mpd]
             [mpd-clj.utils :refer [send-cmd]]))
