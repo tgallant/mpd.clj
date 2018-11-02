6d5b850afd69a4e7655879b8e2d7172035f62afc
Tobias Behr <tobias.behr@holisticon.de>
2018-11-02 21:02:15 +0100

    Updates dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/tgallant/mpd.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [aleph "0.4.0-SNAPSHOT"]
-                 [manifold "0.1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [aleph "0.4.6"]
+                 [manifold "0.1.8"]
                  [gloss "0.2.4"]
-                 [potemkin "0.3.12"]]
+                 [potemkin "0.4.5"]]
   :plugins [[codox "0.8.10"]])
