ffa5c6a7ee29b5d39b4c2ea8979679c563b0ebb5
Tobias Behr <tobias.behr@holisticon.de>
2018-11-05 17:33:21 +0100

    Fixes tests; tests return-obj without running mpd instance

--- test/mpd_clj/core_test.clj
+++ test/mpd_clj/core_test.clj
@@ -2,34 +2,49 @@
   (:require [clojure.test :refer :all]
             [mpd-clj.utils :as u]
             [mpd-clj.db :as db]
-            [manifold.stream :as s]))
+            [manifold.stream :as s]
+            [manifold.deferred :as d]))
 
-(def mpd-server {:host "192.168.1.2" :port 6600})
+(def mpd-server {:host "localhost" :port 6600})
+
+(defn client-with-response [response]
+  (let [client (d/deferred)
+        stream (s/stream (count response))
+        put (partial s/put! stream)]
+    (d/success! client stream)
+    (s/put-all! stream response)
+    client))
 
 ;; utils tests
 
-(deftest client-test
-  (let [c @(u/client (:host mpd-server) (:port mpd-server))
-        req @(s/put! c "status")
-        res @(s/take! c)]
-    (testing "client gets a valid response"
-      (is (= (re-find #"OK MPD" res) "OK MPD")))))
+(comment "this is an integration test"
+         (deftest client-test
+           (let [c @(u/client mpd-server)
+                 req @(s/put! c "status")
+                 res @(s/take! c)]
+             (testing "client gets a valid response"
+               (is (= (re-find #"OK MPD" res) "OK MPD"))))))
 
 (deftest send-cmd-map-test
-  (let [res (u/send-cmd "status" mpd-server)]
+  (let [client (client-with-response ["OK MPD ..." "a: 1" "b: 2" "OK"])
+        res (u/send-cmd "status" client)]
     (testing "testing map response type"
       (is
-        (= (type res) clojure.lang.PersistentHashMap)))))
+        (= (type res) clojure.lang.PersistentArrayMap)
+        (= {:a "1" :b "2"} res)))))
 
 (deftest send-cmd-vec-test
-  (let [res (u/send-cmd "playlistinfo" mpd-server)]
+  (let [client (client-with-response ["OK MPD ..." "a: 1" "a: 2" "OK"])
+        res (u/send-cmd "playlistinfo" client)]
     (testing "testing vector response type"
       (is
-        (= (type res) clojure.lang.PersistentVector)))))
+        (= (type res) clojure.lang.PersistentVector)
+        (= [{:a "1"} {:a "2"}] res)))))
 
 ;; db tests
 
-(deftest track-count-test
-  (let [res-count (count (db/list-tracks "Awake" mpd-server))]
-    (testing "testing db returns proper number of things"
-      (is (= res-count 8)))))
+(comment "this is an integration test" 
+         (deftest track-count-test
+           (let [res-count (count (db/list-tracks "Awake" (u/client mpd-server)))]
+             (testing "testing db returns proper number of things"
+               (is (= res-count 8))))))
